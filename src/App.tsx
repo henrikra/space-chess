d55c7260de9874a37e28b1152092e82687c408d6
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-04-22 13:57:05 +0300

    Add base files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <h1 className="App-title">Welcome to React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
